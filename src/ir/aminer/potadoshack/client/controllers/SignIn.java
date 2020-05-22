d1986acb887bd49f1e7e91e0276dc1a568364e6a
AminerZ <aminerthegamer2000@gmail.com>
2020-05-23 03:42:01 +0430

    Update SignIn.java

--- src/ir/aminer/potadoshack/client/controllers/SignIn.java
+++ src/ir/aminer/potadoshack/client/controllers/SignIn.java
@@ -1,4 +1,15 @@
 package ir.aminer.potadoshack.client.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class SignIn {
+    public Button btn_signin;
+    public TextField txt_username;
+    public TextField txt_password;
+
+    public void signin(ActionEvent actionEvent) {
+
+    }
 }
