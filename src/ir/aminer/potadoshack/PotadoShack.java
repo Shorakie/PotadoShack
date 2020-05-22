6331ddf135444c3faa609f6d041a21da8e3a547d
AminerZ <aminerthegamer2000@gmail.com>
2020-05-23 01:00:09 +0430

    Update PotadoShack.java

--- src/ir/aminer/potadoshack/PotadoShack.java
+++ src/ir/aminer/potadoshack/PotadoShack.java
@@ -2,19 +2,45 @@ package ir.aminer.potadoshack;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PotadoShack extends Application {
+    Map<String, String> pages = new HashMap<>()
+    {{
+        put("main_menu", "layouts/MainMenu.fxml");
+        put("sign_in", "layouts/SignIn.fxml");
+        put("sign_up", "layouts/SignUp.fxml");
+        put("order_registration", "layouts/OrderRegistration.fxml");
+        put("order_view", "layouts/OrderView.fxml");
+        put("cart", "layouts/Cart.fxml");
+    }};
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("layouts/SignIn.fxml"));
         primaryStage.setTitle("Potado Shack");
 
-        Scene primaryScene = new Scene(root);
+        /* Create Empty Scene */
+        Scene primaryScene = new Scene(new Pane());
         primaryStage.setScene(primaryScene);
+
+        /* Awake SceneHandler */
+        SceneHandler.awoke(primaryScene);
+
+        /* Dynamically add pages to SceneHandler */
+        for (Map.Entry<String, String> page : pages.entrySet()){
+            SceneHandler.getInstance().addNode(
+                    page.getKey(),
+                    FXMLLoader.load(getClass().getResource(page.getValue())));
+        }
+
+        /* Set current Page to MainMenu */
+        SceneHandler.getInstance().activeNode("main_menu");
+
         primaryStage.show();
     }
 }
