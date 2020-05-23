f28187a2b38e9b6ccd09c38bf3989acb2e80a806
AminerZ <aminerthegamer2000@gmail.com>
2020-05-24 02:09:15 +0430

    Update PotadoShackServer.java

--- src/ir/aminer/potadoshack/server/PotadoShackServer.java
+++ src/ir/aminer/potadoshack/server/PotadoShackServer.java
@@ -1,18 +1,36 @@
 package ir.aminer.potadoshack.server;
 
-import ir.aminer.potadoshack.client.Client;
+import ir.aminer.potadoshack.core.eventsystem.EventHandler;
+import ir.aminer.potadoshack.core.eventsystem.ListenerMethod;
+import ir.aminer.potadoshack.core.eventsystem.Listener;
+import ir.aminer.potadoshack.core.eventsystem.events.Event;
+import ir.aminer.potadoshack.core.eventsystem.events.SignInEvent;
+import ir.aminer.potadoshack.core.eventsystem.events.SignUpEvent;
+import ir.aminer.potadoshack.server.listeneres.AuthenticationListener;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
 
 public class PotadoShackServer {
+    public static final String SECRET_KEY = "P0t4doS3rver";
     private ServerSocket serverSocket;
     private Socket client;
 
     public void start(int port) throws IOException {
         System.out.println("Starting Server on " + "localhost" + ":" + port);
         serverSocket = new ServerSocket(port);
+
+        /* Create Clients dir if not exists */
+        File clientsDir = new File("./clients/");
+        if (!clientsDir.exists())
+            clientsDir.mkdir();
+
+        /* Awake event handler to create a new instance of it */
+        EventHandler.awake();
+        EventHandler.getInstance().register(new AuthenticationListener());
 
         while (true) {
             client = serverSocket.accept();
