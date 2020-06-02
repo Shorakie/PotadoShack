245fd56db3be5f8f8bc3c824ee220ea02df8492f
AminerZ <aminerthegamer2000@gmail.com>
2020-06-02 17:39:27 +0430

    Create Product.java

---
+++ src/ir/aminer/potadoshack/core/product/Product.java
@@ -0,0 +1,22 @@
+package ir.aminer.potadoshack.core.product;
+
+import java.io.Serializable;
+
+public interface Product extends Serializable {
+    public enum Type{
+        FOOD, DRINK;
+    }
+
+    public interface Category {
+        public String getName();
+        public String getColor();
+    }
+
+    public int getId();
+    public Type getType();
+    public String getName();
+    public int getPrice();
+    public String getImageUrl();
+
+    public Category getCategory();
+}
