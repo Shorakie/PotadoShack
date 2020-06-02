e221aba1d0c2bd8023f00bfaa7554a3f7f6f50ae
AminerZ <aminerthegamer2000@gmail.com>
2020-06-02 17:40:33 +0430

    Update SigninPacket.java

--- src/ir/aminer/potadoshack/core/network/packets/SigninPacket.java
+++ src/ir/aminer/potadoshack/core/network/packets/SigninPacket.java
@@ -1,6 +1,22 @@
 package ir.aminer.potadoshack.core.network.packets;
 
-public class SigninPacket extends Packet{
+public class SignInPacket extends Packet {
+    private final String username;
+    private final String password;
+
+    public SignInPacket(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     @Override
     public int getId() {
         return 1;
