a5045ea9933040414de3119e5e0390f818f250c7
AminerZ <aminerthegamer2000@gmail.com>
2020-06-02 17:39:56 +0430

    Create Cart.java

---
+++ src/ir/aminer/potadoshack/core/order/Cart.java
@@ -0,0 +1,42 @@
+package ir.aminer.potadoshack.core.order;
+
+import ir.aminer.potadoshack.core.product.Product;
+
+import java.io.Serializable;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart implements Serializable {
+    private final Map<Product, Integer> products = new HashMap<>();
+
+    public void setProduct(Product product, final int count) {
+        products.put(product, count);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public void addProduct(Product product) {
+        addProduct(product, 1);
+    }
+
+    public void addProduct(Product product, final int count) {
+        products.computeIfPresent(product, (key, value) -> value + count);
+        products.computeIfAbsent(product, (key) -> count);
+    }
+
+    public Set<Map.Entry<Product, Integer>> getProducts() {
+        return products.entrySet();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder("--------[Cart]---------\n");
+        res.append("Name\t\t\t|Amount\n").append("-----------------------\n");
+        for (Map.Entry<Product, Integer> product : products.entrySet())
+            res.append(product.getKey().getName()).append("\t\t\t|").append(product.getValue()).append("\n");
+        return res.toString();
+    }
+}
