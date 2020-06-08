17421c624f896bd6cfd9e050234da3c0c800d891
AminerZ <aminerthegamer2000@gmail.com>
2020-06-09 00:23:06 +0430

    Update Packet.java
    Added toString support

--- src/ir/aminer/potadoshack/core/network/packets/Packet.java
+++ src/ir/aminer/potadoshack/core/network/packets/Packet.java
@@ -4,4 +4,9 @@ import java.io.Serializable;
 
 public abstract class Packet implements Serializable {
     abstract public int getId();
+
+    @Override
+    public String toString() {
+        return "Packet{id{"+getId()+"}}";
+    }
 }
