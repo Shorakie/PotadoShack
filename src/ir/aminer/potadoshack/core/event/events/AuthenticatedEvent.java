708e2120606c858fb85e9034b069a2a8484c5d4e
AminerZ <aminerthegamer2000@gmail.com>
2020-06-09 00:21:41 +0430

    Update AuthenticatedEvent.java

--- src/ir/aminer/potadoshack/core/event/events/AuthenticatedEvent.java
+++ src/ir/aminer/potadoshack/core/event/events/AuthenticatedEvent.java
@@ -1,22 +1,26 @@
 package ir.aminer.potadoshack.core.event.events;
 
+import ir.aminer.potadoshack.core.auth.simplejwt.JWT;
 import ir.aminer.potadoshack.core.auth.simplejwt.UserPayload;
+import ir.aminer.potadoshack.core.error.Error;
 import ir.aminer.potadoshack.core.network.ClientSocket;
 import ir.aminer.potadoshack.core.network.packets.AuthenticatedPacket;
 import ir.aminer.potadoshack.core.network.packets.Packet;
 import ir.aminer.potadoshack.server.User;
 
-public abstract class AuthenticatedEvent extends Event{
+import java.io.IOException;
+
+public abstract class AuthenticatedEvent extends Event {
     public AuthenticatedEvent(Packet packet, ClientSocket sender) {
         super(packet, sender);
     }
 
-    public User getUser() throws IllegalAccessException {
-        UserPayload payload = ((AuthenticatedPacket)getData()).getJwt().getPayload();
-        User client = User.fromUsername(payload.getUsername());
+    public User getUser() throws IOException {
+        JWT jwt = ((AuthenticatedPacket)getData()).getJwt();
+        User client = User.fromJWT(jwt);
 
-        if (!client.getPassword().equals(payload.getPassword()))
-            throw new IllegalAccessException("Token is unauthorized");
+        if (!client.getPassword().equals(((UserPayload) jwt.getPayload()).getPassword()))
+            getSender().sendError(Error.UNAUTHORIZED_TOKEN);
 
         return client;
     }
