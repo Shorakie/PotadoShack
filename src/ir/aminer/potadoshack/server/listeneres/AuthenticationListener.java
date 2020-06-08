1b640ce048fc11637235f1232bc999257cf274a8
AminerZ <aminerthegamer2000@gmail.com>
2020-06-09 00:24:27 +0430

    Update AuthenticationListener.java

--- src/ir/aminer/potadoshack/server/listeneres/AuthenticationListener.java
+++ src/ir/aminer/potadoshack/server/listeneres/AuthenticationListener.java
@@ -7,7 +7,8 @@ import ir.aminer.potadoshack.core.event.Listener;
 import ir.aminer.potadoshack.core.event.ListenerMethod;
 import ir.aminer.potadoshack.core.event.events.SignInEvent;
 import ir.aminer.potadoshack.core.event.events.SignUpEvent;
-import ir.aminer.potadoshack.core.event.events.ViewCartEvent;
+import ir.aminer.potadoshack.core.event.events.ViewOrdersEvent;
+import ir.aminer.potadoshack.core.network.packets.PrimitivePacket;
 import ir.aminer.potadoshack.core.network.packets.ResponsePacket;
 import ir.aminer.potadoshack.core.utils.HmacSha256;
 import ir.aminer.potadoshack.core.utils.Log;
@@ -51,7 +52,7 @@ public class AuthenticationListener implements Listener {
         JWT jwt = JWT.generate(
                 new UserPayload(client.getUsername(), client.getPassword(), client.getFirstName(), client.getLastName()),
                 PotadoShackServer.SECRET_KEY);
-        event.getSender().sendResponse(jwt.toString(), ResponsePacket.Status.OK);
+        event.getSender().sendResponse(new PrimitivePacket(jwt.toString()), ResponsePacket.Status.OK);
     }
 
     @ListenerMethod
@@ -59,7 +60,13 @@ public class AuthenticationListener implements Listener {
         String username = event.getData().getUsername();
         String password = HmacSha256.hash(PotadoShackServer.SECRET_KEY, event.getData().getPassword());
 
-        User client = User.fromUsername(username);
+        User client = null;
+        try {
+             client = User.fromUsername(username);
+        } catch (IllegalStateException ignored){
+            event.getSender().sendError(Error.USERNAME_NOT_FOUND);
+            return;
+        }
 
         if (!client.getPassword().equals(password)) {
             event.getSender().sendError(Error.WRONG_PASSWORD);
@@ -69,15 +76,6 @@ public class AuthenticationListener implements Listener {
         JWT jwt = JWT.generate(
                 new UserPayload(client.getUsername(), client.getPassword(), client.getFirstName(), client.getLastName()),
                 PotadoShackServer.SECRET_KEY);
-        event.getSender().sendResponse(jwt.toString(), ResponsePacket.Status.OK);
-    }
-
-    @ListenerMethod
-    public void onCartView(ViewCartEvent event) throws IOException {
-        try {
-            System.out.println(event.getUser().getFirstName());
-        } catch (IllegalAccessException e) {
-            event.getSender().sendError(Error.UNAUTHORIZED_TOKEN);
-        }
+        event.getSender().sendResponse(new PrimitivePacket(jwt.toString()), ResponsePacket.Status.OK);
     }
 }
