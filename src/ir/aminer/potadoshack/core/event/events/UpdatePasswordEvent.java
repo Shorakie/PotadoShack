59abd54139cfcf64dbdbbd03c1cd478505b862c9
AminerZ <aminerthegamer2000@gmail.com>
2020-06-15 23:45:42 +0430

    Add UpdatePassword

---
+++ src/ir/aminer/potadoshack/core/event/events/UpdatePasswordEvent.java
@@ -0,0 +1,18 @@
+package ir.aminer.potadoshack.core.event.events;
+
+import ir.aminer.potadoshack.core.network.ClientSocket;
+import ir.aminer.potadoshack.core.network.packets.CancelOrderPacket;
+import ir.aminer.potadoshack.core.network.packets.Packet;
+import ir.aminer.potadoshack.core.network.packets.UpdatePasswordPacket;
+
+public class UpdatePasswordEvent extends AuthenticatedEvent {
+
+    public UpdatePasswordEvent(Packet packet, ClientSocket sender) {
+        super(packet, sender);
+    }
+
+    @Override
+    public UpdatePasswordPacket getData() {
+        return super.getData();
+    }
+}
