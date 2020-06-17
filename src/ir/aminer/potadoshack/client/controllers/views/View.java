9d1c31d6004463cd737d82d2c128e8e865bd5177
AminerZ <aminerthegamer2000@gmail.com>
2020-06-17 18:34:33 +0430

    Update View.java

--- src/ir/aminer/potadoshack/client/controllers/views/View.java
+++ src/ir/aminer/potadoshack/client/controllers/views/View.java
@@ -1,6 +1,9 @@
 package ir.aminer.potadoshack.client.controllers.views;
 
+import com.jfoenix.controls.JFXSnackbar;
+import com.jfoenix.controls.JFXSnackbarLayout;
 import ir.aminer.potadoshack.client.controllers.MainMenu;
+import ir.aminer.potadoshack.core.error.Error;
 
 public abstract class View {
 
@@ -9,9 +12,17 @@ public abstract class View {
     }
 
     protected final MainMenu mainMenu;
+    protected JFXSnackbar snackbar;
 
     public View(MainMenu mainMenu) {
         this.mainMenu = mainMenu;
+        this.snackbar = new JFXSnackbar();
+    }
+
+    protected void errorHandler(Error error) {
+        JFXSnackbarLayout layout = new JFXSnackbarLayout(error.getMessage());
+        layout.getStyleClass().add("error");
+        snackbar.enqueue(new JFXSnackbar.SnackbarEvent(layout));
     }
 
     public abstract Type getType();
